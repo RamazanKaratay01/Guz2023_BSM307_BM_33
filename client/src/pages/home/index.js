57dd043e61a7269209b5966319938ac2386c1951
Racaka <Rmzn-RCK@hotmail.com>
2023-12-27 00:33:29 +0300

    27

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,24 @@
 import styles from './styles.module.css';
+import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
+const Home = ({username, setUsername, room, setRoom, socket}) => {
+  
+  const navigate = useNavigate();
+  
+  const joinRoom = () => {
+    if (room !== '' && username !== '') {
+      socket.emit('join_room', { username, room });
+    }
+    navigate('/chat', { replace: true });
+  };
+  
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h1>{`Chat Odalari`}</h1>
-        <input className={styles.input} placeholder='Kullanici Adi' />
+        <input className={styles.input} placeholder='Kullanici Adi' onChange={(e) => setUsername(e.target.value)} />
 
-        <select className={styles.input}>
+        <select className={styles.input} onChange={(e) => setRoom(e.target.value)}>
           <option>Oda Sec</option>
           <option value='javascript'>Oda 1</option>
           <option value='node'>Oda 2</option>
@@ -15,7 +26,7 @@ const Home = () => {
           <option value='react'>Oda 4</option>
         </select>
 
-        <button className='btn btn-secondary' style={{ width: '100%' }}>Odaya Katil</button>
+        <button className='btn btn-secondary' style={{ width: '100%' }} onClick={joinRoom}>Odaya Katil</button>
       </div>
     </div>
   );
